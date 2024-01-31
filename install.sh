82771943091f15164b968fbfcdcdc3b393fc0829
trigopi <payetalexisg@orange.fr>
2024-01-31 13:58:26 +0200

    install 2

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+chmod u+x ./dependencies/dolphin/install.sh
+chmod u+x ./dependencies/exception/install.sh
+chmod u+x ./dependencies/rest-client/install.sh
+
+chmod u+x ./@tetras-api/node/install.sh
+chmod u+x ./@tetras-api/node/script/python/TrailCamLink/install.sh
+
+echo "installation de dolphin..."
 ./dependencies/dolphin/install.sh
-./dependencies/execption/install.sh
+
+echo "installation de exception..."
+./dependencies/exception/install.sh
+
+echo "installation de rest-client..."
 ./dependencies/rest-client/install.sh
 
+echo "installation de tetras-api..."
 ./@tetras-api/node/install.sh
+
+echo "installation de TrailCamLink..."
 ./@tetras-api/node/script/python/TrailCamLink/install.sh
